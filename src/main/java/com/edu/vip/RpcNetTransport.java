29f33e15c38254feb48f0151f12d729a5872755b
naruto2205200 <perfect_zhangxf@163.com>
2020-04-28 10:24:30 +0800

    bio模式的rpc

--- src/main/java/com/edu/vip/RpcNetTransport.java
+++ src/main/java/com/edu/vip/RpcNetTransport.java
@@ -1,5 +1,10 @@
 package com.edu.vip;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 /**
  * @author naruto
  * @data 2020/4/27.
@@ -16,6 +21,39 @@ public class RpcNetTransport {
     }
 
     public Object send(RpcRequest request) {
-        return null;
+        Socket socket = null;
+        Object obj = null;
+        Object result = null;
+        ObjectInputStream objectInputStream= null;
+        ObjectOutputStream objectOutputStream = null;
+        try {
+            socket = new Socket(host, post);
+            objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            objectOutputStream.writeObject(request);
+            objectOutputStream.flush();
+
+            objectInputStream = new ObjectInputStream(socket.getInputStream());
+            result = objectInputStream.readObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }finally {
+            if (objectInputStream != null) {
+                try {
+                    objectInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (objectOutputStream != null) {
+                try {
+                    objectOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return result;
     }
 }
